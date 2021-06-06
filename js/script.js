e6a3dc7bcc719f0a545b45a50c90c9217b93551b
tamusho329 <show_showed_shown0329@keio.jp>
2021-06-06 15:42:15 +0900

    6/6 noscrollコメントアウト

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ $(function() {
   $('.burger-btn').click(function() {
     $(this).toggleClass('cross');
     $('.header-nav').fadeToggle(300);
-    $('body').toggleClass('noscroll');
+    // $('body').toggleClass('noscroll');
   });
   if ($(window).width() < 768) {
     $('.sp-nav').click(function() {
       $('.burger-btn').toggleClass('cross');
       $('.header-nav').fadeOut();
-      $('body').removeClass('noscroll');
+      // $('body').removeClass('noscroll');
     });
   }
 });
